48ab5aef2ddb8d83ee50a400fa20167b3d903634
Coltrane Willsey <cwillsey06@gmail.com>
2023-01-03 17:17:41 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/*
+
+    ib-embed-helper
+    2023-01-03 [17:12]
+    
+    Reformats Inkbunny submission embeds to
+    be similar to that of e621.net's scheme
+
+    Contributors:
+        cwillsey06
+
+*/
+
